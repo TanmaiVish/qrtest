1f3ab938b70dfe91e48c79472f49de6133200d47
Edward Cragg <drq.11235@gmail.com>
2017-01-19 15:36:03 +0000

    scan with quirc

--- qrtest.c
+++ qrtest.c
@@ -1,14 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "quirc/lib/quirc.h"
 
 #include "data.h"
 
-/*
 int process_quirc(const unsigned char *img_array)
 {
 	struct quirc *qr;
+	uint8_t *image;
+	int w, h;
 	
 	qr = quirc_new();
 	if (!qr) {
@@ -16,15 +18,19 @@ int process_quirc(const unsigned char *img_array)
 		exit(1);
 	}
 
-	if (quirc_resize(qr, 640, 480) < 0) {
+	if (quirc_resize(qr, 4048, 3036) < 0) {
 		printf("Error\n");
 		exit(1);
 	}
 	
+	image = quirc_begin(qr, &w, &h);
+	memcpy(image, img_array, w*h);
+	quirc_end(qr);
+
+	printf("quirc: number of QR codes: %d\n", quirc_count(qr));
 
 	quirc_destroy(qr);
 }
-*/
 
 int main(void)
 {
@@ -34,8 +40,9 @@ int main(void)
 	unsigned long test_image_l = get_test_image_length();
 
 	printf("barcode length: %lu\n", barcode_l);
-	printf("test_image length: %lu\n", test_image_l);
+	printf("test image length: %lu\n", test_image_l);
 
-	//process_quirc(barcode_p);
+	process_quirc(barcode_p);
+	process_quirc(test_image_p);
 }
 

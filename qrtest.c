1fa363d3d15f709f882326d5d6e28651b7a71311
Edward Cragg <drq.11235@gmail.com>
2017-01-19 11:57:23 +0000

    wip

--- qrtest.c
+++ qrtest.c
@@ -1,32 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "quirc/lib/quirc.h"
 
-/* test data */
-#include "testimage.h"
-#include "barcode.h"
+#include "data.h"
 
+/*
 int process_quirc(const unsigned char *img_array)
 {
 	struct quirc *qr;
 	
 	qr = quirc_new();
 	if (!qr) {
-	    perror("Failed to allocate memory");
-	    abort();
+		printf("Error\n");
+		exit(1);
 	}
 
 	if (quirc_resize(qr, 640, 480) < 0) {
-	    perror("Failed to allocate video memory");
-	    abort();
+		printf("Error\n");
+		exit(1);
 	}
 	
 
 	quirc_destroy(qr);
 }
+*/
 
 int main(void)
 {
-	const unsigned char *barcode_p = &barcode;
-	const unsigned char *test_image_p = &test_image;
+	const unsigned char *barcode_p = get_barcode();
+	const unsigned char *test_image_p = get_test_image();
 	unsigned long barcode_l;
 	unsigned long test_image_l;
 
@@ -35,6 +38,6 @@ int main(void)
 	test_image_l = sizeof(*test_image_p);
 	printf("test_image length: %lu\n", test_image_l);
 
-	process_quirc(barcode_p);
+	//process_quirc(barcode_p);
 }
 

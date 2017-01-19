a7683f4893ea05511d6978f33922f19f3df8077e
Edward Cragg <drq.11235@gmail.com>
2017-01-19 14:15:33 +0000

    report array lengths

--- data.c
+++ data.c
@@ -4,12 +4,16 @@
 
 #include "data.h"
 
+#include <stdio.h>
+
 const unsigned char *get_barcode()
 {
+	printf("barcode length: %lu\n", sizeof(barcode));
 	return barcode;
 }
 
 const unsigned char *get_test_image()
 {
+	printf("test image length: %lu\n", sizeof(test_image));
 	return test_image;
 }

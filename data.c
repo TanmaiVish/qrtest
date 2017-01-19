aafc3148da1b2d191b9c99cd8b48c6bbb6ec00a1
Edward Cragg <drq.11235@gmail.com>
2017-01-19 11:58:09 +0000

    add shared library for data

---
+++ data.c
@@ -0,0 +1,15 @@
+/* test data */
+#include "testimage.h"
+#include "barcode.h"
+
+#include "data.h"
+
+const unsigned char *get_barcode()
+{
+	return barcode;
+}
+
+const unsigned char *get_test_image()
+{
+	return test_image;
+}
---
+++ data.h
@@ -0,0 +1,2 @@
+extern const unsigned char *get_barcode();
+extern const unsigned char *get_test_image();
